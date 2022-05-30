a553bf10498f6bcc740f4b2c51d5bf3d8dad2e7b
vicspider2001 <anogwihvictor@gmail.com>
2022-05-31 00:46:36 +0100

    new

---
+++ precache-manifest.95378c4807e36fd35fda66d42235c823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f840332ff2acd537d74429f1ccb9809a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28dc9aacecd5f72f7bf5",
+    "url": "/static/css/2.7f2699be.chunk.css"
+  },
+  {
+    "revision": "4267180848e34b4b9e4a",
+    "url": "/static/css/main.2615b734.chunk.css"
+  },
+  {
+    "revision": "28dc9aacecd5f72f7bf5",
+    "url": "/static/js/2.065da764.chunk.js"
+  },
+  {
+    "revision": "4267180848e34b4b9e4a",
+    "url": "/static/js/main.b2b11a87.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
